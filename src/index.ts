919521fe39d1a73350b908019552df5a46d888ed
Cássia Cavalcante <rafaelsalgues0238@gmail.com>
2025-01-04 15:54:48 -0300

    implemetado query e manipulação delas no js

--- src/index.ts
+++ src/index.ts
@@ -19,8 +19,30 @@ app.use(express.json())
 app.get('/', async (req:Request, res:Response) =>{       //Pool
     
     try {
-        const resposta = await conexao.query('select * from empresas')
-        return res.json(resposta.rows)
+        const query = `
+        select f.id, f.cep, f.rua, f.cidade, f.estado, f.pais, f.empresa_id, e.nome, e.site 
+        from empresas as e 
+        left join filiais as f on e.id = f.empresa_id;
+        `
+        const resposta = await conexao.query(query)
+
+        const filiais = resposta.rows.map(filial =>{
+            return {
+                id: filial.id,
+                cep: filial.cep,
+                rua: filial.rua,
+                cidade: filial.cidade,
+                estado: filial.estadp,
+                pais: filial.pais, 
+                empresa: {
+                    id: filial.empresa_id,
+                    nome: filial.nome,
+                    site: filial.site
+                }
+            }
+        })
+
+        return res.json(filiais)
     } catch (error) {
         const erro = error as Error
         return res.status(400).json(erro.message)
@@ -28,10 +50,38 @@ app.get('/', async (req:Request, res:Response) =>{       //Pool
 })
 
 app.get('/:id', async (req:Request, res:Response) =>{       //Pool
-   try {
     const {id} = req.params
-    const resposta = await conexao.query('select * from empresas where id = $1',[id])
-    return res.json(resposta.rows)
+    try {
+    const query = `
+    select f.id, f.cep, f.rua, f.cidade, f.estado, f.pais, f.empresa_id, e.nome, e.site 
+    from empresas as e 
+    left join filiais as f on e.id = f.empresa_id
+    where e.id =$1;
+    `
+
+   
+    
+    const resposta = await conexao.query(query,[id])
+    const {empresa_id, nome, site} = resposta.rows[0]
+    const filiais = resposta.rows.map(filial =>{
+        return {
+            id: filial.id,
+            cep: filial.cep,
+            rua: filial.rua,
+            cidade: filial.cidade,
+            estado: filial.estadp,
+            pais: filial.pais, 
+            
+        }
+    })
+    const empresa ={
+        id: empresa_id,
+        nome, 
+        site, 
+        filiais 
+    }
+
+    return res.json(empresa)
 
    } catch (error) {
     const erro = error as Error

9e73f186e3f42e2960aca03cf44ec53a07fcdbaa
Cássia Cavalcante <rafaelsalgues0238@gmail.com>
2025-01-04 10:10:54 -0300

    implementado bloco try catch

--- src/coxexao.ts
+++ src/coxexao.ts
@@ -1,14 +1,17 @@
-import {Client, ClientConfig} from 'pg'
+import {Client, Pool, ClientConfig, PoolConfig} from 'pg'
 
-const config:ClientConfig = {
-    host: 'localhost',
-    port: 5432,
-    user: 'postgres',
-    password: 'postgres',
-    database: 'aula_conexao_nodejs_postgres'
+const config:PoolConfig = {
+    host: process.env.PGHOST,
+    port: Number(process.env.PGPORT),
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+    
 }
 
-export const client = new Client(config)
+//export const client = new Client(config)
+
+export const conexao = new Pool(config)
 
 
 
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,42 @@
 import express, { Request, Response } from 'express'
 import 'dotenv/config'
-import { client } from './coxexao'
+import { conexao } from './coxexao'
 
 
 const app = express()
 
 app.use(express.json())
 
-app.get('/', async (req:Request, res:Response):Promise <any> =>{
-    await client.connect()
-    const resposta = await client.query('select * from empresas')
+// app.get('/', async (req:Request, res:Response) =>{   //Client
+//     await client.connect()
+//     const resposta = await client.query('select * from filiais')
 
-    await client.end()
+//     await client.end()
 
-    return res.json(resposta)
+//     return res.json(resposta)
+// })
+
+app.get('/', async (req:Request, res:Response) =>{       //Pool
+    
+    try {
+        const resposta = await conexao.query('select * from empresas')
+        return res.json(resposta.rows)
+    } catch (error) {
+        const erro = error as Error
+        return res.status(400).json(erro.message)
+    }
+})
+
+app.get('/:id', async (req:Request, res:Response) =>{       //Pool
+   try {
+    const {id} = req.params
+    const resposta = await conexao.query('select * from empresas where id = $1',[id])
+    return res.json(resposta.rows)
+
+   } catch (error) {
+    const erro = error as Error
+    return res.status(400).json(erro.message)
+   }
 })
 
 app.listen(process.env.PORT, ()=>{
